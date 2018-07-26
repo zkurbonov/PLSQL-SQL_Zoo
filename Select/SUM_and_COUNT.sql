48da9a5d9f4d4fe59ed7f5930b5d422efbaf2b92
JohnSlack <Zafarchik29@mail.ru>
2018-07-26 16:29:12 +0500

    Added some fixes

--- Select/SUM_and_COUNT.sql
+++ Select/SUM_and_COUNT.sql
@@ -50,3 +50,11 @@
 	FROM world
 	GROUP BY continent
 	HAVING SUM(population) >= 100E6;
+	
+-- 9. List the continents that have a total population of at least 100 million.
+
+	SELECT continent 
+	FROM world
+	GROUP BY continent
+	HAVING SUM(population) >= 100E6;
+
